01258e09d7a141a5c386b23858e4bfd7f7b5475e
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-28 21:09:22 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "rg-group-name" {
+  type = string
+  #default = "01000000-0000-4000-8000-000030080200"
+  
+}
+variable "rg-group-location" {
+  type = string
+  #default = "01000000-0000-4000-8000-000030080200" 
+}
