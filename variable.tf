b8adb2321d2310dbb42a8a5bd7905b17322af0fc
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-28 22:21:01 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
-variable "rg-eastus2" {
+variable "rg-eastus2-name" {
   type = string
   #default = "01000000-0000-4000-8000-000030080200"  
 }
-variable "rg-centralus" {
+variable "rg-centralus-name" {
   type = string
   #default = "01000000-0000-4000-8000-000030080200"  
 }
