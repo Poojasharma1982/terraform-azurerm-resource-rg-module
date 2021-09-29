84d4c6b52d75a0547d75fbcb784bb3304bbd909d
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-28 22:14:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
-resource "azurerm_resource_group" "rg-new" {
-  name     = var.rg-group-name
-  location = var.rg-group-location
+resource "azurerm_resource_group" "rg-eastus2" {
+  name     = var.rg-eastus2-name
+  location = "eastus2"
+  tags = {
+      Environment = "Test"
+      Service     = "Example"
+      month = "September"
+      day = "tuesday"
+    }
+}
+resource "azurerm_resource_group" "rg-centralus" {
+  name     = var.rg-centralus-name
+  location = "centralus"
   tags = {
       Environment = "Test"
       Service     = "Example"
