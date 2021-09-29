5a3baaf62ef070c89fe77d6f95c2bfb66e0f0935
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-28 22:17:24 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
-output "rg-id" {
-  value = azurerm_resource_group.rg-new.id
+output "rg-id-1" {
+  value = azurerm_resource_group.rg-eastus2.id
+}
+output "rg-id-2" {
+  value = azurerm_resource_group.rg-centralus.id
 }
